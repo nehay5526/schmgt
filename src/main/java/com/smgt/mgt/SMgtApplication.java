4e022da059de1e007817c3785383621aa3d3d3b7
neha.yadav <neha.yadav@jogosoft.in>
2022-08-29 07:51:24 +0000

    firstCommit

---
+++ src/main/java/com/smgt/mgt/SMgtApplication.java
@@ -0,0 +1,13 @@
+package com.smgt.mgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SMgtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SMgtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smgt/mgt/SMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smgt.mgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SMgtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
