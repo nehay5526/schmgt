1b693e675598433caba85eea1f591fc1920168a4
neha.yadav <neha.yadav@jogosoft.in>
2022-08-30 13:53:32 +0000

    2nd commit

--- src/main/java/com/smgt/mgt/SMgtApplication.java
+++ src/main/java/com/smgt/mgt/SMgtApplication.java
@@ -8,6 +8,7 @@ public class SMgtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SMgtApplication.class, args);
+        System.out.println("Hello world");
     }
 
 }
